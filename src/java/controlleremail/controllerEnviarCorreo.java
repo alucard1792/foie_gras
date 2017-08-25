6f3c221a80dea700192888deb544a9b81f275f73
ORLAN07 <orlandorivasrubio@live.com>
2017-08-24 20:10:19 -0500

    cambios en la vista email

---
+++ src/java/controlleremail/controllerEnviarCorreo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlleremail;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ORLANDO
+ */
+@Named(value = "controllerEnviarCorreo")
+@SessionScoped
+public class controllerEnviarCorreo implements Serializable {
+
+    /**
+     * Creates a new instance of controllerEnviarCorreo
+     */
+    
+    
+    
+    
+    public controllerEnviarCorreo() {
+    }
+    
+    
+    public void enviarEmail(){
+       controllerEmail cemail = new controllerEmail("correofixedup@gmail.com", "fixedupsena", "orubio04@misena.edu.co");
+        cemail.enviar(cemail.getAsunto(), cemail.getContenido()); 
+    }
+    
+    
+
+}
