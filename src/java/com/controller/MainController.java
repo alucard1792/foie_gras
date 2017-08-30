79a48b33d27fddf26a39529ddd60b026d531a2d3
ORLAN07 <orlandorivasrubio@live.com>
2017-08-30 00:54:57 -0500

    implementacion de el contactenos funcional

---
+++ src/java/com/controller/MainController.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+import com.mailsender.MailSender;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Niyaz-laptop
+ */
+@ManagedBean(name = "MainController")
+@SessionScoped
+public class MainController implements Serializable {
+
+    private String fromMail;
+    private String username;
+    private String password;
+    private String toMail;
+    private String subject;
+    private String message;
+
+    public String getFromMail() {
+        return fromMail;
+    }
+
+    public void setFromMail(String fromMail) {
+        this.fromMail = fromMail;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToMail() {
+        return toMail;
+    }
+
+    public void setToMail(String toMail) {
+        this.toMail = toMail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void send() {
+        try {
+            MailSender mailSender=new MailSender();
+            mailSender.sendMail("correofixedup@gmail.com", "correofixedup@gmail.com", "fixedupsena", "correofixedup@gmail.com", subject, message);
+        } catch (Exception e) {
+        }
+    }
+}
