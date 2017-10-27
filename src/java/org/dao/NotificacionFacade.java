0d629c4e40fe9eecf23dee24efc03a8d08456a6f
alucard1792 <dz9217@hotmail.com>
2017-10-27 15:14:18 -0500

    DAOs notificacion

---
+++ src/java/org/dao/NotificacionFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.dao;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.entidades.Notificacion;
+
+/**
+ *
+ * @author David
+ */
+@Stateless
+public class NotificacionFacade extends AbstractFacade<Notificacion> implements NotificacionFacadeLocal {
+
+    @PersistenceContext(unitName = "fixed_up_prototipo_v2PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public NotificacionFacade() {
+        super(Notificacion.class);
+    }
+    
+}
